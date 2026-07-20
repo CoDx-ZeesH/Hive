f7bddf9ae7546c49d1b4a24487e19883b5a26126
Mohammad Zeeshan <mohammad.zeeshan.tech@gmail.com>
2026-07-20 12:22:59 +0530

    config updated

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,6 @@
+import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
-/**
- * Prisma v7 configuration.
- *
- * Connection URLs have moved here from schema.prisma (breaking change in v7).
- * The datasource.url is used by the Prisma client at runtime.
- * The datasource.shadowDatabaseUrl is used during migrations.
- *
- * See: https://pris.ly/d/config-datasource
- */
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
